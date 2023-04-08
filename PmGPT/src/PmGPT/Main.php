af08d3627f5a0858d21a4b0bace87d052fdd133c
GLX20 <79281788+galaxytwenty@users.noreply.github.com>
2023-04-08 15:59:08 +0200

    Update code to work with gpt-3.5-turbo model
    Add reminder chat history

--- PmGPT/src/PmGPT/Main.php
+++ PmGPT/src/PmGPT/Main.php
@@ -11,10 +11,16 @@ use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener {
 
-protected Config $config;
+    protected Config $config;
+    protected array $conversation = [];
 
     public function onEnable() :void {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
+    public function onLoad(): void
+    {
         $this->saveResource("config.yml");
         $this->config = new Config($this->getDataFolder() . "config.yml", 2);
         if($this->config->get("OpenAiApiKey") === ""){
@@ -29,10 +35,11 @@ protected Config $config;
         $message = $event->getMessage();
         if(str_contains(strtolower($message), "chatgpt")) {
             $question = preg_replace('/^chatgpt\s+/i', '', $message);
+            var_dump($question);
             $response = $this->getGPTResponse($question);
             $delayTicks = 2;
             $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function() use ($player, $delayTicks, $response) : void {
-                $player->sendMessage("§a".$response);
+                $player->sendMessage("§4ChatGPT:\n§a".$response);
             }), $delayTicks);
 
         }
@@ -40,36 +47,49 @@ protected Config $config;
 
 
     function getGPTResponse($question) {
-        $temperature = 0.2;
-        $url = 'https://api.openai.com/v1/completions';
-        $auth_token = $this->config->get("OpenAiApiKey");
-        $data = array(
-            'model' => 'text-davinci-003',
-            'prompt' => $question,
-            'temperature' => $temperature,
-            'max_tokens' => 150,
-            'frequency_penalty' => 0,
-            'presence_penalty' => 0.6,
+        $ch = curl_init();
+
+
+        $query = implode("\n", $this->conversation) . "\nUser: $question";
+        $url = 'https://api.openai.com/v1/chat/completions';
+
+        $api_key = $this->config->get("OpenAiApiKey");
+
+        $post_fields = array(
+            "model" => "gpt-3.5-turbo",
+            "messages" => array(
+                array(
+                    "role" => "user",
+                    "content" => $query
+                )
+            ),
+            "max_tokens" => (int)$this->config->get("maxTokens"),
+            "temperature" => (int)$this->config->get("temperature")
         );
-        $payload = json_encode($data);
-        $headers = array(
+
+        $header  = [
             'Content-Type: application/json',
-            'Authorization: Bearer ' . $auth_token,
-            'Content-Length: ' . strlen($payload)
-        );
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); //ONLY TEST Security warning WHY no SSL
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //ONLY TEST Security warning WHY no SSL
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            'Authorization: Bearer ' . $api_key
+        ];
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_fields));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+
         $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error: ' . curl_error($ch);
+        }
         curl_close($ch);
-        $response = json_decode($result, true);
+
+        $response = json_decode($result);
         $answer = "";
-        if (!empty($response['choices'][0]['text'])) {
-            $answer = trim($response['choices'][0]['text']);
+        if (!empty($response->choices[0]->message->content)) {
+            $answer = trim($response->choices[0]->message->content);
+            $this->conversation[] = "user:".$question."\n".$answer;
         }
         return $answer;
     }
